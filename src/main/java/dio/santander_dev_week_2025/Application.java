74136ad710192bf864e4a5db6b1d7116561c3cf9
Anderson-G-Silva <caricasp@hotmail.com>
2025-11-06 21:34:03 -0300

    fix CORS

--- src/main/java/dio/santander_dev_week_2025/Application.java
+++ src/main/java/dio/santander_dev_week_2025/Application.java
@@ -1,10 +1,13 @@
 package dio.santander_dev_week_2025;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 @EntityScan (basePackages = "dio.santander_dev_week_2025.domain.model")
 @EnableFeignClients
 @SpringBootApplication
